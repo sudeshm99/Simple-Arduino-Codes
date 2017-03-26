648dd3a94c22ebb1aad933e428bcf48791860f13
sudeshm99 <kgs.madu99@gmail.com>
2017-03-26 21:45:49 +0530

    button control funtion

---
+++ butonControl/butonControl.ino
@@ -0,0 +1,18 @@
+const int led = 9;
+const int button = 2;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(led, OUTPUT);
+  pinMode(button, INPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if(digitalRead(button) == HIGH){
+      digitalWrite(led, HIGH);
+    }else{
+        digitalWrite(led, LOW);
+      }
+}
