ffe83235e0adb24deb8e1a137f425da70057ce2b
sudeshm99 <kgs.madu99@gmail.com>
2017-03-27 11:50:31 +0530

    changing button switching

--- buttonSwitch/buttonSwitch.ino
+++ buttonSwitch/buttonSwitch.ino
@@ -1,12 +1,12 @@
-const int led = 9;
-const int button = 2;
+const int led = 9;//led connected port is port 9
+const int button = 2;//button connected port is port 2
 boolean lastButton = LOW;
 boolean currentButton = LOW;
 boolean ledOn = false; 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(led, OUTPUT);
-  pinMode(button, INPUT);
+  pinMode(led, OUTPUT);//initialized led(9) port is the port that gives out put
+  pinMode(button, INPUT);// initialized button(2) is the port that gets input from user
 }
 boolean debounce(boolean last){
     boolean current = digitalRead(button);//get the current boolean value of button
